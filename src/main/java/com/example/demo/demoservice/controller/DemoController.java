d33b92efd639e8bb8747d8300e0c6619d77fd16b
Venkatesan Mahendran <venkatesan.mahendran@gmail.com>
2019-09-10 12:54:55 +0100

    Add an incoming connection

--- src/main/java/com/example/demo/demoservice/controller/DemoController.java
+++ src/main/java/com/example/demo/demoservice/controller/DemoController.java
@@ -2,13 +2,21 @@ package com.example.demo.demoservice.controller;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+import java.io.IOException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.demoservice.model.Name;
+
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 @RequestMapping(produces = APPLICATION_JSON_VALUE)
 @RestController
@@ -21,6 +29,33 @@ public class DemoController {
 		System.out.println("DemoController::getNames()");
 
 		return new Name("Venkatesan", "Mahendran");
+
+	}
+
+	@GetMapping(path = "/ships/{shipName}/names")
+	@ResponseStatus(HttpStatus.OK)
+	public String getCaptainName(@PathVariable("shipName") String shipName) {
+		System.err.println(shipName);
+
+		OkHttpClient client = new OkHttpClient();
+		
+		String hostUrl = System.getProperty("outgoing.connection.url");
+		
+		System.out.println(hostUrl);
+
+		String url = hostUrl + "/names";
+		
+		System.out.println(url);
+
+		Request request = new Request.Builder().url(url).build();
+
+		try (Response response = client.newCall(request).execute()) {
+			return response.body().string();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "error";
+		}
 
 	}
 }
--- starter_local.sh
+++ starter_local.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 SPRING_PROFILE=${SPRING_PROFILE:-development}
-SHIP_APPLICATION_URL=${SHIP_APPLICATION_URL:-http://blackpearlserv-service:9090/blackpearlserv/v1.0}
+SHIP_APPLICATION_URL=${SHIP_APPLICATION_URL:-http://127.0.0.1:9090/blackpearlserv/v1.0}
 
 java -Xmx768m \
     -Dspring.profiles.active=${SPRING_PROFILE} \
     -Doutgoing.connection.url=${SHIP_APPLICATION_URL} \
-    -jar demo-service-1.0.0.jar
+    -jar target/demo-service-1.0.0.jar
